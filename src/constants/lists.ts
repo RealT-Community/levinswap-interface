d530fee063360ff92f9a36d942c63ec32f3f2336
Lint Action <lint-action@samuelmeuli.com>
2021-01-22 10:21:06 +0000

    Fix code style issues with ESLint

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmdfJoc8VXmMZq1u7hdMR7iMGsQS55H6XLAvLYY8fdic4u?filename=Tokenlist'
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://ipfs.io/ipfs/QmdfJoc8VXmMZq1u7hdMR7iMGsQS55H6XLAvLYY8fdic4u?filename=Tokenlist'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
