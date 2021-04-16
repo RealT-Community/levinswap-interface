2f60b69b053d60bf235977225b117fa03238e8ac
LucaSilverhand <levinswap0@gmail.com>
2021-04-16 21:16:35 +0400

    tokenlists

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,16 +1,16 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
 const LEVINSWAP_LIST =
-  'https://ipfs.io/ipfs/QmVjmLAgkWuNxHAE9Um9KuwEJmTEk82LzdEqLnfiZ5NQpR?filename=levinswap-default.tokenlist.json'
+  'https://ipfs.io/ipfs/QmUWxthidUYXUJ2kiZLLPxkMKYDAinnpA591R3SRN6wufs?filename=levinswap-default.tokenlist.json'
 const LEVINSWAP_STABLECOIN_LIST =
   'https://ipfs.io/ipfs/QmWrhnRTCQ8CgSoNmHV6WsneLLhErouD4fQPpSaqhsibpD?filename=levinswap-stablecoin-tokenlist.json'
 const REALT_LIST = 'https://api.realt.community/v1/tokenList'
 const REN_LIST =
-  'https://ipfs.io/ipfs/QmS55aGgkVoZLj87qP2VZMrnmv5PupMsDVvUp6tBHfReLF?filename=ren-default.tokenlist.json'
+  'https://ipfs.io/ipfs/QmSqYWWdzfngm57int8AesJr1APRBBYxC8knmYC9rN4ECj?filename=ren-default.tokenlist.json'
 const SYNTHETIX_LST =
-  'https://ipfs.io/ipfs/QmbZvrV7YsF6eKP8umoauqi7FFTG4KgtQBjTeLfVFJpxZj?filename=synthetix-default.tokenlist.json'
+  'https://ipfs.io/ipfs/QmYeWjmKW4tQ6JfCG9XUkVa9CFBAUY36Z1W9w1iM6Bx4ne?filename=synthetix-default.tokenlist.json'
 const BSC_LIST =
-  'https://ipfs.io/ipfs/QmbFFNszCPSUZktXYdjVd7wSaqnMxeXedrQWC5zcuEk8pv?filename=bsc-default.tokenlist.json'
+  'https://ipfs.io/ipfs/QmcT2RnZ2tJcpfxaruy1EkKAF6aGCRrVsNFfn5oSCQLPQJ?filename=bsc-default.tokenlist.json'
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
 
