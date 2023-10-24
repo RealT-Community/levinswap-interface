05404b5484c45eca57ab7d4a466f4365fb0a6816
Sigri44 <sigri44@hotmail.fr>
2023-10-24 22:53:37 +0200

    Fix invalid assets logo

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -8,7 +8,7 @@ import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
 export const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/dapps/xdai/assets/${address}/logo.png`
+  `./images/tokens/${address}.png`
 
 const StyledxDaiLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
